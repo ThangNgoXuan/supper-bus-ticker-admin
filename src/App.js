2e50d2209aa7e213444a913df2661a7e82c81c51
NgoXuanThang <thang.nx@novaeforce.vn>
2022-08-21 15:27:26 +0700

    init source

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import {Button} from 'antd';
+
+
+function App() {
+  return (
+    <div>
+      admin bus ticket
+      <Button type='primary'>Button</Button>
+    </div>
+  );
+}
+
+export default App;
