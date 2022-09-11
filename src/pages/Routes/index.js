868397a13494224ef639d3974a3725f034a7542f
NgoXuanThang <ngoxuanthangnyc@gmail.com>
2022-09-11 19:32:20 +0700

    page route

--- src/pages/Routes/index.js
+++ src/pages/Routes/index.js
@@ -1,7 +1,75 @@
-import React from 'react'
+import { Button, Table, Typography } from "antd";
+import React from "react";
 
 export default function Routes() {
+  const { Title } = Typography;
+
+  const columns = [
+    {
+        title: "Mã chuyến xe",
+        dataIndex: "id",
+        key: "id",
+    },
+    {
+        title: "Tuyến đường",
+        dataIndex: "route",
+        key: "route",
+    },
+    {
+        title: "Xe chạy",
+        dataIndex: "coach",
+        key: "coach",
+    },
+    {
+        title: "Loại xe",
+        dataIndex: "typeCoach",
+        key: "typeCoach",
+    },
+    {
+        title: "Trạng thái",
+        dataIndex: "route",
+        key: "route",
+    },
+    {
+        title: "Ghế đã đặt",
+        dataIndex: "book",
+        key: "book",
+    },
+    {
+        title: "Ghế chưa đặt",
+        dataIndex: "noBook",
+        key: "noBook",
+    },
+    {
+        dataIndex: "function",
+        key: "function",
+        render: () => (
+            <div className="p-news_table_button">
+              <Button type="primary">Xem</Button>
+              <Button type="primary">Đặt vé</Button>
+            </div>
+          ),
+    },
+  ]
+
+  const data = new Array(30).fill({
+    id: '1',
+    route: 'Bình Dương => Bình Định',
+    coach: '77A 24359',
+    typeCoach: "Xe limousin",
+    status: "Đang chạy",
+    book: 33,
+    noBook: 2,
+  })
+
   return (
-    <div>Routes</div>
-  )
+    <div className="p-routes">
+      <div className="p-routes">
+        <Title level={2}>Quản lí tuyến xe</Title>
+      </div>
+      <div className="p-routes_table">
+        <Table columns={columns} dataSource={data}/>
+      </div>
+    </div>
+  );
 }
