303a65d22f3f66d33f2500c4782132138edea80b
NgoXuanThang <ngoxuanthangnyc@gmail.com>
2023-01-05 07:23:53 +0700

    qqq

--- src/pages/Booking/index.js
+++ src/pages/Booking/index.js
@@ -9,10 +9,18 @@ import {
   Select,
   Typography,
 } from "antd";
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import routerApi from '../../api/routerApi'
+import useValues from "../../hooks/useValues";
 export default function Booking() {
   const [open, setOpen] = useState(false);
+  const [values, setValues] = useValues({
+    selectedFrom: "",
+    selectedTo: "",
+    selectedTime: new Date(),
+    listRoutes: [],
+    listRoutesCustom: [],
+  });
 
   const onChange = (value) => {
     console.log(`selected ${value}`);
@@ -42,7 +50,7 @@ export default function Booking() {
   const { Title } = Typography;
 
   const originData = [];
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 10; i++) {
     originData.push({
       id: i.toString(),
       name: `Edrward ${i}`,
@@ -51,17 +59,116 @@ export default function Booking() {
     });
   }
 
+  useEffect(() => {
+    routerApi.getAllRoute().then((res) => {
+      if (res?.status) {
+        setValues({
+          listRoutes: res?.data,
+          listRoutesCustom: customRoutes(res?.data),
+          selectedFrom: {
+            label: res?.data[0]?.from?.name,
+            value: res?.data[0]?.from?._id,
+          },
+          selectedTo: {
+            label: res?.data[0]?.to?.name,
+            value: res?.data[0]?.to?._id,
+          },
+        });
+      }
+    });
+  }, []);
+
+  const handleReset = () => {
+    setValues({
+      selectedFrom: {
+        label: values?.listRoutes[0].from?.name,
+        value: values?.listRoutes[0].from?._id,
+      },
+      selectedTo: {
+        label: values?.listRoutes[0].to?.name,
+        value: values?.listRoutes[0].to?._id,
+      },
+      listRoutesCustom: customRoutes(values.listRoutes),
+    });
+  };
+
+
+  const handleChoose = (type, value) => {
+    let from = values.listRoutesCustom.from;
+    let to = values.listRoutesCustom.to;
+    if (type === "from") {
+      to = values.listRoutes
+        .filter((item) => item.from._id === value.value)
+        .map((item2) => ({
+          label: item2?.to?.name,
+          value: item2?.to?._id,
+        }));
+      setValues({
+        listRoutesCustom: {
+          to: to,
+        },
+      });
+    } else if (type === "to") {
+      from = values.listRoutes
+        .filter((item) => item.to._id === value.value)
+        .map((item2) => ({
+          label: item2?.from?.name,
+          value: item2?.from?._id,
+        }));
+      setValues({
+        listRoutesCustom: {
+          from: from,
+        },
+      });
+    }
+  };
+
+  const customRoutes = (routes) => {
+    let from = [];
+    let to = [];
+
+    if (Array.isArray(routes)) {
+      //ban đầu
+      routes.forEach((element) => {
+        if (!from.find((item) => item?.value === element?.from?._id)) {
+          from.push({ label: element?.from?.name, value: element?.from._id });
+        }
+        if (!to.find((item) => item?.value === element?.to?._id)) {
+          to.push({ label: element?.to?.name, value: element?.to?._id });
+        }
+      });
+    }
+    return { from, to };
+  };
+
+
+  console.log("data", values.listRoutesCustom)
+
   return (
     <div className="p-booking">
       <div className="p-booking_filter">
         <div className="p-booking_filter_schedule">
-          <Title level={3}>Chọn tuyến</Title>
+          <Title level={3}>Điểm đi</Title>
           <Select
-            showSearch
-            placeholder="Select a person"
+            placeholder="Chọn điểm đi"
+            optionFilterProp="children"
+            options={values.listRoutesCustom.from}
+            onChange={onChange}
+            onSearch={onSearch}
+            filterOption={(input, option) =>
+              option.toLowerCase().includes(input.toLowerCase())
+            }
+          >
+          </Select>
+        </div>
+        <div className="p-booking_filter_schedule">
+          <Title level={3}>Điểm đến</Title>
+          <Select
+            placeholder="Chọn điểm đến"
             optionFilterProp="children"
             onChange={onChange}
             onSearch={onSearch}
+            options={values.listRoutesCustom.to}
             filterOption={(input, option) =>
               option.toLowerCase().includes(input.toLowerCase())
             }
@@ -95,23 +202,6 @@ export default function Booking() {
         </div>
         <div className="p-booking_filter_place">
           <Title level={3}>Chọn điểm đón</Title>
-          <Select
-            showSearch
-            placeholder="Select a person"
-            optionFilterProp="children"
-            onChange={onChange}
-            onSearch={onSearch}
-            filterOption={(input, option) =>
-              option.toLowerCase().includes(input.toLowerCase())
-            }
-          >
-            <Option value="jack">Nhơn Hạnh</Option>
-            <Option value="lucy">Nhơn Phong</Option>
-            <Option value="tom">Nhơn An</Option>
-          </Select>
-        </div>
-        <div className="p-booking_filter_place">
-          <Title level={3}>Chọn xe</Title>
           <Select
             showSearch
             placeholder="Select a person"
